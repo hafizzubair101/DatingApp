27a996c06de8a041124dabdda22cce9320209b59
Zubair <hafiz.zubair@ssasoft.com>
2022-04-05 13:39:29 +0500

    chapter 9 09 Building the User Interface

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -2,3 +2,28 @@ export interface User {
     username: string;
     token: string;
 } 
+
+// let data: number | string = 42;
+
+// data = "10";
+
+// interface car {
+//     color: string;
+//     model: string;
+//     topSpeed?: number;
+// }
+
+// const car1: car = {
+//     color: 'red',
+//     model: 'BMW'
+// }
+
+// const car2: car = {
+//     color: 'red',
+//     model: 'Mercedes',
+//     topSpeed: 100
+// }
+
+// const multiply = (x: number, y: number) => {
+//     return x* y;
+// }
